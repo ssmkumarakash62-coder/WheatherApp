636f30e48fba0e1e471be812b76b202a7dfec84a
Akash Kumar <ssmkumarakash62@gmail.com>
2026-05-14 01:46:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const apiKey = "4a5437758c80b3088ed7011bdd436420"; // Apni API key yahan dalein
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector("button");
+searchBtn.addEventListener("click", () => {    checkWeather(searchBox.value);
+});
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        // Dynamic Icons Logic
+        if(data.weather[0].main == "Clouds") {
+            weatherIcon.src = "https://cdn-icons-png.flaticon.com/512/1146/1146869.png";
+        } else if(data.weather[0].main == "Clear") {
+            weatherIcon.src = "https://cdn-icons-png.flaticon.com/512/869/869869.png";
+        } else if(data.weather[0].main == "Rain") {
+            weatherIcon.src = "https://cdn-icons-png.flaticon.com/512/3351/3351086.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
